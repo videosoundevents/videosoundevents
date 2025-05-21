b2aaa34492b8265c4a56e620f293ba5288d88119
NNLmix7 <142530612+NNLmix7@users.noreply.github.com>
2025-05-21 23:23:38 +0300

    Update and rename send-email.js to send-email.ts

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,8 +1,7 @@
-// File: /api/send-email.js
-
 import nodemailer from 'nodemailer';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
